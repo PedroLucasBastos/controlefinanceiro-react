47fa6275f0de51e303dc2d794aa6ca1c2a3204fe
Pedro Bastos <pedro19792001@gmail.com>
2024-12-12 18:15:01 -0300

    fix: corrigindo schema do prisma

---
+++ app/transactions/page.tsx
@@ -0,0 +1,14 @@
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>{transaction.name} </div>
+      ))}
+    </div>
+  );
+};
+export default TransactionsPage;
