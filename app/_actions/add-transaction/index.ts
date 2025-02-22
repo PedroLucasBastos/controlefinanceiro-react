f94d1345ab381f9295ce4eab2f11c64c619050f9
Pedro Bastos <pedro19792001@gmail.com>
2025-02-22 19:34:40 -0300

    feat: adiciona funcionalidade de edição de transações com diálogo de atualização

--- app/_actions/add-transaction/index.ts
+++ app/_actions/add-transaction/index.ts
@@ -11,6 +11,7 @@ import { AddtransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
 interface AddTransactionParams {
+  id?: string;
   name: string;
   amount: number;
   type: TransactionType;
@@ -27,8 +28,12 @@ export const AddTransaction = async (params: AddTransactionParams) => {
     throw new Error("Unauthorized");
   }
 
-  await db.transaction.create({
-    data: { ...params, userId },
+  await db.transaction.upsert({
+    where: {
+      id: params.id,
+    },
+    update: { ...params, userId },
+    create: { ...params, userId },
   });
   revalidatePath("/transactions");
 };
