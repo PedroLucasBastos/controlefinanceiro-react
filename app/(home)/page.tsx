01e2ab38e648fa58e64cbfc76820e36996520474
Pedro Bastos <pedro19792001@gmail.com>
2025-03-04 20:18:51 -0300

    feat: adiciona componente DateSelect e integra na página inicial para seleção de mês

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,17 +2,35 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
+import DateSelect from "./_components/date-select";
+import { isMatch } from "date-fns";
 
-const Home = async () => {
+interface HomeProps {
+  searchParams: {
+    mouth: string;
+  };
+}
+
+const Home = async ({ searchParams: { mouth } }: HomeProps) => {
   const { userId } = await auth();
 
   if (!userId) {
     redirect("/login");
   }
+  const mouthIsInvalid = !mouth || !isMatch(mouth, "MM");
+  if (mouthIsInvalid) {
+    redirect("/?mouth=01");
+  }
   return (
     <>
       <Navbar />
-      <SummaryCards />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <DateSelect />
+        </div>
+        <SummaryCards mouth={mouth} />
+      </div>
     </>
   );
 };
