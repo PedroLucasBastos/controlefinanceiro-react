1d9cc02b2f3903d6a0ee37f9309669bf21c2ed93
Pedro Bastos <pedro19792001@gmail.com>
2025-03-04 18:19:36 -0300

    feat: adiciona componente Navbar e integra em páginas principais

---
+++ app/subscription/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../_components/navbar";
+
+const Subscription = () => {
+  return (
+    <>
+      <Navbar />
+      <div>teste</div>
+    </>
+  );
+};
+
+export default Subscription;
