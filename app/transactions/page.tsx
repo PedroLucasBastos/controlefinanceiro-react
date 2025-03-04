0e9cb84d3b3c6e8edacc59f8b5f074a7c84ddd2b
Pedro Bastos <pedro19792001@gmail.com>
2025-03-04 18:19:36 -0300

    feat: adiciona componente Navbar e integra em páginas principais

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,22 +2,26 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/addTransactionButton";
+import Navbar from "../_components/navbar";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div className="space-y-6 p-6">
-      {/*Titilo e botão*/}
-      <div className="flex w-full items-center justify-between p-6">
-        <h1 className="text-2xl font-bold">Trasações</h1>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        {/*Titilo e botão*/}
+        <div className="flex w-full items-center justify-between p-6">
+          <h1 className="text-2xl font-bold">Trasações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable
+          columns={transactionColumns}
+          data={JSON.parse(JSON.stringify(transactions))}
+        />
       </div>
-      <DataTable
-        columns={transactionColumns}
-        data={JSON.parse(JSON.stringify(transactions))}
-      />
-    </div>
+    </>
   );
 };
 export default TransactionsPage;
