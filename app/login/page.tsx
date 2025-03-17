655af9c216ef7b4d288f1486f4a4ca9d03a72c2d
Pedro Bastos <pedro19792001@gmail.com>
2025-03-17 15:48:09 -0300

    fix: responsividade da tela de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,29 +12,36 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="grid h-full grid-cols-2">
-      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Image
-          src="./logo.svg"
-          alt="budgetbuddy"
-          width={250}
-          height={250}
-          className="mb-8 ml-auto mr-auto flex items-center"
-        />
-        <h1 className="mb-3 text-4xl font-bold">Bem-Vindo!</h1>
-        <p className="mb-8 text-muted-foreground">
+    <div className="grid h-full min-h-screen grid-cols-1 md:grid-cols-2">
+      {/* Seção de login */}
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center px-4 py-8">
+        <div className="flex flex-col items-center">
+          <Image
+            src="./logo.svg"
+            alt="budgetbuddy"
+            width={170}
+            height={170}
+            className="mb-6"
+          />
+          <h1 className="mb-3 text-center text-3xl font-bold md:text-4xl">
+            Bem-Vindo!
+          </h1>
+        </div>
+        <p className="mb-8 text-center text-gray-500">
           BudgetBuddy é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
-          <Button variant="outline">
+          <Button className="mx-auto w-full max-w-[300px]" variant="outline">
             <LogInIcon className="mr-2" />
             Fazer Login
           </Button>
         </SignInButton>
       </div>
-      <div className="relative h-full w-full">
+
+      {/* Imagem lateral */}
+      <div className="relative hidden h-full w-full md:block">
         <Image
           src="/login.png"
           alt="Faça login"
@@ -45,4 +52,5 @@ const LoginPage = async () => {
     </div>
   );
 };
+
 export default LoginPage;
